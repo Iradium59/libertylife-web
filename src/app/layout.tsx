806fb784eacecbfe9577413e4ac57be2739ed726
Iradium59 <samuel.definance003.jeu@gmail.com>
2024-12-11 14:51:18 +0100

    ajout donnée analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "LibertyLife",
-  description: "Fivem Serveur",
+  description: "Bienvenue sur Liberty City, la ville où vous serez libre d'être qui vous voulez ! Rejoignez le conflit opposant l'ombre et la lumière",
+  authors: [{name: "Iradium", url: "https://samuel-def.fr"}],
+  keywords: ["LibertyLife", "Liberty City", "Liberty Life", "LibertyCity", "Liberty", "City", "Life", "Roleplay", "RP", "GTA", "V", "FiveM", "Five", "M", "GTA V", "GTA FiveM", "GTA RP", "GTA Roleplay", "Roleplay GTA"]
 };
 
 export default function RootLayout({
@@ -16,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <Analytics/>
         <SpeedInsights />
       </body>
     </html>
